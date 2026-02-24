932e54904d8e03b8d7ffdde5b7f995aad3feaea6
Elysia315K <Elysia315K@noreply.gitcode.com>
2026-02-25 00:38:34 +0800

    企业日程与考勤智能提醒平台前后端初始化
    1. 后端初始化
     - 自定义异常
     - 响应包装类
     - 全局异常处理器
     - 请求包装类
     - 全局跨域配置
    2. 前端初始化
     - 全局通用布局
     - 修改路由配置，跳转，高亮同步
     - 引入axios工具库，自动生成请求代码工具库
     - 全局自定义请求

---
+++ sa-reminder-backend/src/main/java/com/workspace/sareminderbackend/SaReminderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.workspace.sareminderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaReminderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaReminderBackendApplication.class, args);
+    }
+
+}
---
+++ sa-reminder-backend/src/test/java/com/workspace/sareminderbackend/SaReminderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workspace.sareminderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaReminderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
